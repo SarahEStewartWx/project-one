e9af2e05bb4a2bd86da003eee073dfed56afd36e
Bruce Millspaugh <brucemillspaugh@gmail.com>
2024-10-18 21:34:51 -0400

    FIGURED OUT THE MODAL YAY

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 
 //--------------------------------------
+// Modal Code
+var modal = document.getElementById('id01');
+                window.onclick = function(event) {
+                    if (event.target == modal) {
+                        modal.style.display = "none";
+                    }
+                }
+//--------------------------------------
 // the code below is for the weather api
 //--------------------------------------
 // function to fetch weather from API
@@ -61,47 +69,3 @@ dropdownMenu.addEventListener("change", () => {
 window.addEventListener("load", checkLocalStorage);
 
 //-----------------------------------------------------------------------------------------
-
-// JS for Modal supplied by Bulma
-document.addEventListener('DOMContentLoaded', () => {
-  // Functions to open and close a modal
-  function openModal($el) {
-    $el.classList.add('is-active');
-  }
-
-  function closeModal($el) {
-    $el.classList.remove('is-active');
-  }
-
-  function closeAllModals() {
-    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
-      closeModal($modal);
-    });
-  }
-
-  // Add a click event on buttons to open a specific modal
-  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
-    const modal = $trigger.dataset.target;
-    const $target = document.getElementById(modal);
-
-    $trigger.addEventListener('click', () => {
-      openModal($target);
-    });
-  });
-
-  // Add a click event on various child elements to close the parent modal
-  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-    const $target = $close.closest('.modal');
-
-    $close.addEventListener('click', () => {
-      closeModal($target);
-    });
-  });
-
-  // Add a keyboard event to close all modals
-  document.addEventListener('keydown', (event) => {
-    if(event.key === "Escape") {
-      closeAllModals();
-    }
-  });
-});
